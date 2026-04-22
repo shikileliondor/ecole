756ffe90006ae198b14a0eb87706f5635ef40e9a
Beyam <160472943+shikileliondor@users.noreply.github.com>
2026-04-22 15:03:59 +0000

    Fix modal transition wrappers to prevent DOM removeChild errors

--- resources/js/Components/Modal.tsx
+++ resources/js/Components/Modal.tsx
@@ -4,7 +4,7 @@ import {
     Transition,
     TransitionChild,
 } from '@headlessui/react';
-import { PropsWithChildren } from 'react';
+import { Fragment, PropsWithChildren } from 'react';
 
 export default function Modal({
     children,
@@ -33,14 +33,14 @@ export default function Modal({
     }[maxWidth];
 
     return (
-        <Transition show={show} leave="duration-200">
+        <Transition as={Fragment} show={show} leave="duration-200">
             <Dialog
                 as="div"
                 id="modal"
                 className="fixed inset-0 z-50 flex transform items-center overflow-y-auto px-4 py-6 transition-all sm:px-0"
                 onClose={close}
             >
-                <TransitionChild
+                <TransitionChild as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0"
                     enterTo="opacity-100"
@@ -51,7 +51,7 @@ export default function Modal({
                     <div className="absolute inset-0 bg-gray-500/75" />
                 </TransitionChild>
 
-                <TransitionChild
+                <TransitionChild as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     enterTo="opacity-100 translate-y-0 sm:scale-100"
